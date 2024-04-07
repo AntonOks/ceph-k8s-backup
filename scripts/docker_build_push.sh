9523badd1fdcf53bac74975bd36af42cabb0d473
Remi Rampin <remi@rampin.org>
2024-04-07 10:49:05 -0400

    Set mode=max for Docker build caching

--- scripts/docker_build_push.sh
+++ scripts/docker_build_push.sh
@@ -7,7 +7,8 @@ IMAGE=ghcr.io/remram44/ceph-k8s-backup:$VERSION
 
 docker buildx build --pull \
     . \
-    --cache-from type=registry,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache --cache-to type=registry,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache,oci-mediatypes=false \
+    --cache-from type=registry,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache \
+    --cache-to type=registry,mode=max,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache,oci-mediatypes=false \
     --platform linux/amd64,linux/arm/v7,linux/arm64 \
     --push --tag $IMAGE
 
